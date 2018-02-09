fce09f8255c45f555d0285aa794aac5044b41a1e
RaymondYang <651208679@qq.com>
2018-02-09 02:03:25 -0800

    observer module test passed, push parts to be settled

--- Observable.js
+++ Observable.js
@@ -11,12 +11,19 @@ class ObserverList {
   }
   add(observer) {
     // todo add observer to list
+    return this.observerList.push(observer)
   }
   remove(observer) {
     // todo remove observer from list
+    for (let i = 0; i < this.observerList.length; i++) {
+      if (this.observerList[i] === observer) {
+        this.observerList.splice(i, 1)
+      }
+    }
   }
   count() {
     // return observer list size
+    return this.observerList.length
   }
 }
 
@@ -26,12 +33,18 @@ class Subject {
   }
   addObserver(observer) {
     // todo add observer
+    this.observers.add(observer)
   }
   removeObserver(observer) {
     // todo remove observer
+    this.observers.remove(observer)
   }
   notify(...args) {
     // todo notify
+    let observerCount = this.observers.count();
+    for(let i=0; i < observerCount; i++){
+      this.observers.observerList[i].update(...args);
+    }
   }
 }
 
